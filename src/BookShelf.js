da779c5de56a617d7fa17d81f1d98e6ab1b83a85
Aviral Gupta <aviral@Avirals-MacBook-Pro.local>
2017-09-01 13:30:57 +0530

    BookShelf connected to Backend API

--- src/BookShelf.js
+++ src/BookShelf.js
@@ -2,7 +2,7 @@ import  React, {Component} from 'react'
 
 function camelize(str) {
     return str.replace(/(?:^\w|[A-Z]|\b\w)/g, function(letter, index) {
-        return index == 0 ? letter.toLowerCase() : letter.toUpperCase();
+        return index === 0 ? letter.toLowerCase() : letter.toUpperCase();
     }).replace(/\s+/g, '');
 }
 
@@ -11,14 +11,14 @@ class BookShelf extends Component {
 
 
     render() {
+        const shelfType = camelize(this.props.type)
         let booksInShelf = this.props.books.filter((book) =>
-            book.shelf === camelize(this.props.title)
+            book.shelf === shelfType
         )
-        console.log(booksInShelf);
-
+        console.log(booksInShelf)
         return (
             <div className="bookshelf">
-                <h2 className="bookshelf-title">{this.props.title}</h2>
+                <h2 className="bookshelf-title">{this.props.type}</h2>
                 {booksInShelf.length ? (
                     <div className="bookshelf-books">
                     <ol className="books-grid">
@@ -28,7 +28,7 @@ class BookShelf extends Component {
                                 <div className="book-top">
                                     <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
                                     <div className="book-shelf-changer">
-                                        <select>
+                                        <select value={shelfType} onChange={(event) => this.props.onChangeShelf(book, event)}>
                                             <option value="none" disabled>Move to...</option>
                                             <option value="currentlyReading">Currently Reading</option>
                                             <option value="wantToRead">Want to Read</option>
@@ -45,7 +45,7 @@ class BookShelf extends Component {
                     </ol>
                     </div>
                 ) : (
-                    <div>{this.props.title} is empty</div>
+                    <div>{this.props.type} is empty</div>
                 )}
             </div>
         )
--- src/BookShelves.js
+++ src/BookShelves.js
@@ -6,20 +6,31 @@ class BookShelves extends Component {
     state = {
         books: []
     }
-
     componentDidMount() {
         BooksAPI.getAll().then((books) => {
             this.setState({ books })
         })
     }
+    changeShelf = (book, event) => {
+        const updatedShelf = event.target.value
+        let books = this.state.books
 
+        books.forEach((element, index) => {
+            if(element.id === book.id) {
+                books[index].shelf = updatedShelf
+            }
+        })
+
+        this.setState({books})
+        BooksAPI.update(book, updatedShelf)
+    }
 
     render() {
         return (
             <div>
-                <BookShelf title="Currently Reading" books={this.state.books} />
-                <BookShelf title="Want to Read" books={this.state.books}/>
-                <BookShelf title="Read" books={this.state.books}/>
+                <BookShelf type="Currently Reading" books={this.state.books} onChangeShelf={this.changeShelf} />
+                <BookShelf type="Want to Read" books={this.state.books} onChangeShelf={this.changeShelf} />
+                <BookShelf type="Read" books={this.state.books} onChangeShelf={this.changeShelf} />
             </div>
         )
     }
